d348de199ad151b9dca03d9de9fbfc99cda4b406
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-02-16 14:23:00 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+
+
+
+// console.log (cats)
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){
+    const copyofcats = [...cats];
+    copyofcats.push(name)
+    return copyofcats
+}
+function prependCat(name){
+    const copyofcats = [...cats];
+    copyofcats.unshift(name)
+    return copyofcats
+
+}
+
+function removeLastCat(name){
+    const copyofcats = [...cats]
+    copyofcats.pop()
+    return copyofcats
+
+
+}
+function removeFirstCat(name) {
+const copyofcats = [...cats]
+copyofcats.shift()
+return copyofcats
+}
